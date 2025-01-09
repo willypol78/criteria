886fb049f88700b69aaf749cacbf1da0b62f169d
Willy <guillermo.mir@gmail.com>
2025-01-09 12:00:39 +0100

    Test the EQUALS and = in URI

--- criteria-from-http-servlet-request/src/test/java/com/codely/criteria/from/CriteriaFromHttpServletRequestUriConverterTest.java
+++ criteria-from-http-servlet-request/src/test/java/com/codely/criteria/from/CriteriaFromHttpServletRequestUriConverterTest.java
@@ -40,9 +40,10 @@ class CriteriaFromHttpServletRequestUriConverterTest {
 		CriteriaFromHttpServletRequestUriConverter converter = new CriteriaFromHttpServletRequestUriConverter();
 
 		// DQL: name = "John" AND NOT(age < 18 OR age > 65)
-		MockHttpServletRequest mockRequest = new MockHttpServletRequest();
+		MockHttpServletRequest mockRequest = new MockHttpServletRequest(); //%3D
 		mockRequest.setRequestURI(
-				"/api/v1/users?f[0][f]=name&f[0][o]=%3D&f[0][v]=John&f[1][o]=AND&f[2][o]=NOT&f[3][o]=(&f[4][f]=age&f[4][o]=%3C&f[4][v]=18&f[5][o]=OR&f[6][f]=age&f[6][o]=%3E&f[6][v]=65&f[7][o]=)&orderBy=name&orderType=ASC&page=1&size=10"
+				"/api/v1/users?f[0][f]=name&f[0][o]=EQUAL&f[0][v]=John&f[1][o]=AND&f[2][o]=NOT&f[3][o]=" +
+						"(&f[4][f]=age&f[4][o]=LT&f[4][v]=18&f[5][o]=OR&f[6][f]=age&f[6][o]=%3E&f[6][v]=65&f[7][o]=)&orderBy=name&orderType=ASC&page=1&size=10"
 		);
 
 		// When
